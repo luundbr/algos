41f5dc80f6a6729eb6b3733b2354b92198944d68
Dan <ashen-cloud@proton.me>
2022-06-16 14:09:11 +0200

    same results as torch

--- backprop.js
+++ backprop.js
@@ -1,9 +1,4 @@
-const T = m => m[0].map((_, i) => m.map(row => row[i]))
-
-const matmul = (a, b) => a.map((row, i) => b[0].map((_, j) =>
-      row.reduce((acc, _, n) => acc + a[i][n] * b[n][j], 0)
-    )
-  )
+const nj = require('numjs')
 
 class Linear {
   weights = []
@@ -22,35 +17,41 @@ class Linear {
         )
       }
     }
+
+    this.weights = nj.array(this.weights)
   }
 
   pass(x) {
-    const y = []
-
-    // todo: bias
-    for (const ws of this.weights) {
-      let r = 0
-      for (const inp of x) {
-        for (const w of ws) {
-          r += w * inp
-        }
-      }
-      y.push(parseFloat(r.toFixed(4)))
-      r = 0
-    }
-
+    x = nj.array(x)
+    const y = x.dot(this.weights.T)
     return y
   }
 
 }
 
-const relu = xa => xa.map(x => x > 0 ? x : 0)
+const relu = xa => nj.array(xa.tolist().map(x => x > 0 ? x : 0))
+
+const relu_grad = xa => nj.array(xa.tolist().map(x => x > 0 ? 1 : 0))
+
+const SE = (xa, tg) => xa.tolist().map(x => 0.5 * ((tg - x)**2))
+  .reduce((a, c) => a + c, 0)
+
+// const MSE_w = (xa, tg) => 2 * (xa.map(x => (x - tg)**2)
+//  .reduce((a, c) => a + c, 0)) / xa.length
+
+
+const MSE = (xa, tg) => 1/xa.tolist().length * (xa.tolist().map((x) => (x - tg)**2)
+  .reduce((a, c) => a + c, 0))
+
+const MSE_grad = (xa, tg) => 2/xa.tolist().length * (xa.tolist().map((x) => x - tg)
+  .reduce((a, c) => a + c, 0))
+
 
 class NN {
   constructor() {
-    this.input = new Linear(2, 4)
-    this.hidden = new Linear(4, 4)
-    this.output = new Linear(4, 2)
+    this.input = new Linear(2, 2)
+    this.hidden = new Linear(2, 2)
+    this.output = new Linear(2, 2)
   }
 
   forward(x) {
@@ -59,9 +60,22 @@ class NN {
     x = this.hidden.pass(x)
     x = relu(x)
     x = this.output.pass(x)
+    return x
   }
 }
 
 const nn = new NN()
 
-console.log(nn)
+const EPOCHS = 3
+
+for (let i = 0; i < EPOCHS; i++) {
+  const data = [0.3, 0.5]
+  const label = [0.7, 0.4]
+
+  const out = nn.forward(data)
+
+  const loss = MSE(out, label)
+
+  // console.log('out', out)
+  // console.log('loss', loss)
+}
